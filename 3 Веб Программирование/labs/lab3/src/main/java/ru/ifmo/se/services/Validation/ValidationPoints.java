244fd37af13f7c15345decc294f8bb2057db517f
Ran4er <dd@bloom.com>
2025-02-22 06:53:20 +0300

    add new changes in lab3

--- 3 Веб Программирование/labs/lab3/src/main/java/ru/ifmo/se/services/Validation/ValidationPoints.java
+++
@@ -1,8 +0,0 @@
-package ru.ifmo.se.services.Validation;
-
-import java.io.Serializable;
-
-public interface ValidationPoints extends Serializable {
-
-    boolean validate(double x, double y, double r);
-}
