8ec5392cf9c16e3b5fbd579a87eeb250f848b632
Ran4er <dd@bloom.com>
2025-02-22 06:53:20 +0300

    add new changes in lab3

--- 3 Веб Программирование/labs/lab3/src/main/java/ru/ifmo/se/repositories/HitResultRepository.java
+++ 3 Веб Программирование/labs/lab3/src/main/java/ru/ifmo/se/repositories/HitResultRepository.java
@@ -1,36 +1,34 @@
 package ru.ifmo.se.repositories;
 
-import com.google.gson.Gson;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.faces.context.FacesContext;
-import jakarta.inject.Inject;
-import jakarta.inject.Named;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import com.google.gson.Gson;
 import jakarta.transaction.Transactional;
 import ru.ifmo.se.models.Attempt;
-import ru.ifmo.se.services.AreaChekcing.AreaCheck;
-import ru.ifmo.se.services.AreaChekcing.AreaCheckQualifier;
-import ru.ifmo.se.services.Validation.ValidatePointsQualifier;
-import ru.ifmo.se.services.Validation.ValidationPoints;
+import ru.ifmo.se.services.AreaCheck;
+import ru.ifmo.se.services.AreaCheckQualifier;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-//Controller - Service - Repository
-//
-
 @Named("attemptRepository")
 @ApplicationScoped
 public class HitResultRepository implements Serializable {
-
     private static final int LATEST_ATTEMPTS_COUNT = 10;
 
     @PersistenceContext
     private EntityManager entityManager;
+
+    @Inject
+    @AreaCheckQualifier
+    private AreaCheck areaCheck;
 
     public List<Attempt> getAttemptsList(int start, int count) {
         return entityManager.createQuery("select attempt from Attempt attempt", Attempt.class)
@@ -44,6 +42,14 @@ public class HitResultRepository implements Serializable {
                 .setFirstResult(firstResultIndex).setMaxResults(LATEST_ATTEMPTS_COUNT).getResultList();
     }
 
+    @Transactional
+    public Attempt addAttempt(Attempt attempt) {
+        areaCheck.checkHit(attempt);
+        entityManager.merge(attempt);
+        entityManager.flush();
+        return attempt;
+    }
+
     public int getAttemptsCount() {
         return entityManager.createQuery("select count(*) from Attempt", Number.class).getSingleResult().intValue();
     }
@@ -53,5 +59,49 @@ public class HitResultRepository implements Serializable {
         entityManager.createQuery("delete from Attempt").executeUpdate();
     }
 
+    @Transactional
+    public void addAttemptFromJsParams(int currentR) {
+        final Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
 
+        try {
+            var x = Double.parseDouble(params.get("x"));
+            var y = Double.parseDouble(params.get("y"));
+            var graphR = Double.parseDouble(params.get("r"));
+
+            final Attempt attemptBean = new Attempt(
+                    x / graphR * currentR,
+                    y / graphR * currentR,
+                    currentR
+            );
+            addAttempt(attemptBean);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String collectToJson(Function<? super Attempt, Double> getter) {
+        return new Gson().toJson(getLatestAttemptsList().stream().map(getter).collect(Collectors.toList()));
+    }
+
+    public String getX() {
+        return collectToJson(Attempt::getX);
+    }
+
+    public String getY() {
+        return collectToJson(Attempt::getY);
+    }
+
+    public String getR() {
+        return collectToJson(Attempt::getR);
+    }
+
+    public String getHit() {
+        return new Gson().toJson(getLatestAttemptsList().stream().map(Attempt::isResult).collect(Collectors.toList()));
+    }
+
+    public String getPointsCoordinates() {
+        return new Gson().toJson(
+                getLatestAttemptsList().stream().map(Attempt::getCoordinates).collect(Collectors.toList())
+        );
+    }
 }
--- 3 Веб Программирование/labs/lab3/src/main/java/ru/ifmo/se/services/AreaChekcing/AreaCheck.java
+++
@@ -1,9 +0,0 @@
-package ru.ifmo.se.services.AreaChekcing;
-
-import ru.ifmo.se.models.Attempt;
-
-import java.io.Serializable;
-
-public interface AreaCheck extends Serializable {
-    void checkHit(Attempt hitResultBean);
-}
